7985ca2de33f29b23c33942c96385fdfd3a7de76
mikenchi <mikenti337564@gmail.com>
2024-08-16 19:42:05 +0900

    [add]characterbaseをとりあえず完成させました

--- Assets/Kobayashi/Script/CharacterBase.cs
+++ Assets/Kobayashi/Script/CharacterBase.cs
@@ -3,17 +3,23 @@ using UnityEngine;
 
 public abstract class CharacterBase : MonoBehaviour
 {
+    [SerializeField,Tooltip("永続バフでの数値の上昇値")] float _buffUp = 0.1f;
+    [SerializeField,Tooltip("今回バフでの数値の上昇値")] float _oneTimeBuffUp = 0.1f;
+    [SerializeField, Tooltip("デバフの数値の上昇値")] float _debuffUp = 0.1f;
     public string _name;
     public float _hp;
-
-    public List<CardBase> _cards;
-    public List<Buff> _buff;
+    public float _attackpower;
+    public List<CardBase> _deck;
+    public List<Buff> _buff;//永続バフのリスト
+    public List<Buff> _oneTimeBuff;//今回バフのリスト
+    public List<Buff> _debuff;//デバフのリスト
+    public Buff _characterBuff;//キャラクターの属性値メインプレイヤーの属性はNoneの想定です
     abstract public void CardExecution(CardBase card, GameObject attackObject);
     virtual public void SetStatus(Data enemydata)
     {
         _name = enemydata._name;
         _hp = enemydata._maxHp;
-        _cards = enemydata._cardData;
+        _deck = enemydata._cardData;
     }
     public void SetBuff(Buff buff, int count)
     {
@@ -24,38 +30,51 @@ public abstract class CharacterBase : MonoBehaviour
             SetBuff(buff, count - 1);
         }
     }
-    public float Damage(float damage)
+    public void Attack(CharacterBase enemy)
     {
-        int damageBuff=0;
-        int damageDebuff = 0;
-        foreach (var buff in _buff)
+        //forループでエネミーの属性値と対象となるバフを検索して計算式にわり当てはめています。
+        for(int i= 0; i< 3; i++)
         {
-            switch (buff)
+            if (enemy._characterBuff == (Buff)i)
             {
-                case Buff.DamageBuff:
-                    damageBuff++;
-                    break;
-                case Buff.DamageDebuff:
-                    damageDebuff++;
-                    break;
-                case Buff.All:
-
-                    break;
+                float buff = 0;
+                float onetimebuff = 0;
+                float debuff = 0;
+                for (int j = 0; j < _buff.Count; j++)
+                {
+                    if (_buff[j] == (Buff)i)
+                    {
+                        buff += _buffUp;
+                    }
+                }
+                for(int h = 0; h < _oneTimeBuff.Count; h++)
+                {
+                    if (_buff[h] == (Buff)i)
+                    {
+                        onetimebuff += _oneTimeBuffUp;
+                    }
+                }
+                debuff += _debuffUp * _debuff.Count;
+                enemy._hp -= ((_attackpower + buff) * onetimebuff) - debuff;
+                break;
             }
         }
-        _hp -= damage + damageBuff-damageDebuff;
-        Debug.Log($"攻撃後HP{_hp}ダメージ{damage + damageBuff-damageDebuff}");
-        if (_hp < 0)
-        {
-            Debug.Log("死亡した");
-        }
-        return damage;
+    }
+    public void BuffReset()
+    {
+        _buff.Clear();
+    }
+    public void OneTimeBuffReset()
+    {
+        _oneTimeBuff.Clear();
     }
    
 }
 public enum Buff
 {
-    DamageBuff,
-    DamageDebuff,
-    All
+    None,
+    Red,
+    Green,
+    Blue
 }
+
--- Assets/tsutsumi/CardBase.cs
+++ Assets/tsutsumi/CardBase.cs
@@ -1,65 +1,99 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public abstract class CardBase : MonoBehaviour , IDragHandler , IBeginDragHandler, IPointerUpHandler, IPointerDownHandler
 {
+  
     GameObject _desk;
     GameObject _nulldesk;
+    GameObject _selectdesk;
+    CharacterBase _player;
+    CharacterBase[]_enemy;
     [SerializeField] Buff _buff;
     [SerializeField] float _buffcount;
-    [SerializeField] float _damage;
     [SerializeField] AttackPaturn _attackPaturn;
+    [SerializeField] float _damage;
     [SerializeField] int _attackCount = 1;
+    [SerializeField] bool _isPlayer = true;
+    [SerializeField] bool _test = true;
     void Start()
     {
         _desk = GameObject.FindWithTag("Desk");
         _nulldesk = GameObject.FindWithTag("DeskOut");
+        _selectdesk = GameObject.FindWithTag("SelectDesk");
+        if (!_test)
+        {
+            GameObject[] enemy = GameObject.FindGameObjectsWithTag("Enemy");
+            _player = GameObject.FindWithTag("Player").GetComponent<CharacterBase>();
+            enemy = new GameObject[enemy.Length];
+            for (int i = 0; i < enemy.Length; i++)
+            {
+                _enemy[i] = enemy[i].GetComponent<CharacterBase>();
+            }
+        }
         transform.SetParent(_desk.transform);
     }
     void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
     {
-        transform.SetParent(_nulldesk.transform);
+        if (_isPlayer)
+        {
+            transform.SetParent(_nulldesk.transform);
+        }
     }
     void IDragHandler.OnDrag(PointerEventData eventData)
     {
-        transform.position = eventData.position;
+        if (_isPlayer)
+        {
+            transform.position = eventData.position;
+        }
+      
     }
 
     GameObject GetCurrentDeck(PointerEventData eventData)
     {
-        var results = new List<RaycastResult>();
-        EventSystem.current.RaycastAll(eventData, results);
-        RaycastResult result = default;
-        foreach (var item in results)
-        {
-            if (item.gameObject.CompareTag("Desk"))
+            var results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(eventData, results);
+            RaycastResult result = default;
+            foreach (var item in results)
             {
-                result = item;
-                break;
+                if (item.gameObject.CompareTag("Desk"))
+                {
+                    result = item;
+                    break;
+                }
             }
-        }
-        return result.gameObject;  
+            return result.gameObject;   
     }
     public void OnPointerDown(PointerEventData eventData)
     {
     }
     public void OnPointerUp(PointerEventData eventData)
     {
-        GameObject desk = GetCurrentDeck(eventData);
-        if (desk)
+        if (_isPlayer)
         {
-            transform.SetParent(desk.transform);
+            GameObject desk = GetCurrentDeck(eventData);
+            if (desk)
+            {
+                transform.SetParent(desk.transform);
+            }
+            else
+            {
+                transform.SetParent(_selectdesk.transform);
+            }
         }
-        else 
-        {
-            CardUse();
-            CardUseEvent();
-        }
+       
     }
-    public virtual void CardUse()
+    public void CardUse()
+    {
+        CardUsing();
+        CardUseEvent();
+    }
+    public virtual void CardUsing()
     {
 
     }
