571972a94262dd5179d36a4db43114b0c134f000
kobakoGlhf <147145211+kobakoGlhf@users.noreply.github.com>
2024-08-17 14:55:22 +0900

    [fix]バトルマネージャーの変更、ray部分をBattelManager に統合

--- Assets/Kobayashi/Script/BattleManager.cs
+++ Assets/Kobayashi/Script/BattleManager.cs
@@ -1,58 +1,63 @@
+using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
     [SerializeField] int _firstDraw;
-    [SerializeField] Transform _waitCardParent;
+    [SerializeField] Transform _waitingCardListParent;
     public static BattleManager _instance;
-    bool _turnChange;
+    bool _EndTarn;
     Trun _trun;
+    Trun _nextTrun;
     CharacterBase _player;
     List<CharacterBase> _enemyList = new List<CharacterBase>();
-    Queue<CardBase> _playerDeck = new Queue<CardBase> ();
+    Queue<CardBase> _playerDeck = new Queue<CardBase>();
     void Start()
     {
 
     }
     void Update()
     {
-        if (_trun == Trun.Start)
+        switch (_trun)
         {
-            SetData();
-            NextTrun(Trun.ChoiceCard);
-        }
-        else if (_trun == Trun.ChoiceCard && _turnChange)
-        {
-            _turnChange = false;
-            DrawCard();
-        }
-        else if (_trun == Trun.UseCard && _turnChange)
-        {
-            UseCard();
-            NextTrun(Trun.Attack);
-        }
-        else if (_trun == Trun.Attack && _turnChange)
-        {
-            Attack();
-            _turnChange = false;
-        }
-        else if (_trun == Trun.EnemyAttack && _turnChange)
-        {
-            EnemyAttack();
-            NextTrun(Trun.ChoiceCard);
+            case Trun.Start:
+                SetData();
+                _trun = Trun.None;
+                break;
+            case Trun.ChoiceCard:
+                DrawCard(_firstDraw);
+                _trun = Trun.None;
+                break;
+            case Trun.UseCard:
+                UseCard();
+                _trun = Trun.None;
+                break;
+            case Trun.PlayerAttack:
+                PlayerAttack();
+                _trun = Trun.None;
+                break;
+            case Trun.EnemyAttack:
+                EnemyAttack();
+                _trun = Trun.None;
+                break;
+            default:
+                if (_EndTarn)
+                {
+                    NextTrun(_nextTrun);
+                }
+                break;
         }
     }
     void NextTrun(Trun trun)
     {
-        _turnChange = true;
+        _EndTarn = false;
         _trun = trun;
     }
     public void SetData()
     {
         CharacterBase[] character = GameObject.FindObjectsOfType<CharacterBase>();
-        foreach(var chara in character)
+        foreach (var chara in character)
         {
             if (chara.gameObject.tag == "Player")
             {
@@ -63,33 +68,57 @@ public class BattleManager : MonoBehaviour
                 _enemyList.Add(chara);
             }
         }
-        DeckShuffle();
+        _playerDeck = DeckShuffle(_player._deck);
     }
-    void DrawCard()
+    void DrawCard(int DrawCount)
     {
-        for(int i = 0; i < _firstDraw; i++)
+        for (int i = 0; i < DrawCount; i++)
         {
             Instantiate(_playerDeck.Dequeue());
+            Debug.Log(_playerDeck.Count);
+            if (_playerDeck.Count >= 0)
+            {
+                _playerDeck = DeckShuffle(_player._deck);
+            }
         }
     }
     void UseCard()
     {
-        CardBase[] cards = _waitCardParent.GetComponentsInChildren<CardBase>();
-        foreach(var playCard in cards)
+        CardBase[] cards = _waitingCardListParent.GetComponentsInChildren<CardBase>();
+        foreach (var playCard in cards)
         {
             playCard.CardUse();
         }
     }
-    void Attack()
+    IEnumerator PlayerAttack()
     {
-        if (_enemyList == null)
+        while (true)
         {
-            Victory();
+            switch (_player._attackPattern)
+            {
+                case AttackPattern.All:
+                    foreach (var enemy in _enemyList)
+                    {
+                        _player.Attack(enemy);
+                    }
+                    break;
+                default:
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        _player.Attack(GetMouseClickEnemy());
+                    }
+                    break;
+            }
+            if (_enemyList == null)
+            {
+                Victory();
+            }
+            yield return new WaitForEndOfFrame();
         }
     }
     void EnemyAttack()
     {
-        foreach(var enemy in _enemyList)
+        foreach (var enemy in _enemyList)
         {
             enemy.Attack(_player);
         }
@@ -106,28 +135,51 @@ public class BattleManager : MonoBehaviour
     {
         Debug.Log("victory");
     }
-    void DeckShuffle()
+    Queue<T> DeckShuffle<T>(List<T> DeckData)
     {
-        List<CardBase> playerCards = new List<CardBase>();//playerのカードリスト（仮想）
-        foreach (var Card in _player._deck)
+        List<T> cardsList = new List<T>();//playerのカードリスト（仮想）
+        Queue<T> deckQueue = new Queue<T>();
+        foreach (var Card in DeckData)
         {
-            playerCards.Add(Card);
+            cardsList.Add(Card);
         }
-        for (int i = 0; i < _player._deck.Count; i++)
+        for (int i = 0; i < DeckData.Count; i++)
         {
-            var j = Random.Range(0, _player._deck.Count);
-            var temp = playerCards[i];
-            playerCards[i] = playerCards[j];
-            playerCards[j] = temp;
-            _playerDeck.Enqueue(playerCards[i]);
+            var j = Random.Range(0, DeckData.Count);
+            var temp = cardsList[i];
+            cardsList[i] = cardsList[j];
+            cardsList[j] = temp;
+            deckQueue.Enqueue(cardsList[i]);
         }
+        return deckQueue;
+    }
+    CharacterBase GetMouseClickEnemy()
+    {
+#nullable enable
+        CharacterBase? hitGameObject;
+#nullable disable
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 100);
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
+        if (hit.transform != null)
+        {
+            hitGameObject = hit.transform.gameObject.GetComponent<CharacterBase>();
+            if (hitGameObject != null)
+            {
+                Debug.Log(hitGameObject);
+                return hitGameObject;
+            }
+        }
+        return null;
     }
 }
+
 public enum Trun
 {
+    None,
     Start,
     ChoiceCard,
     UseCard,
-    Attack,
+    PlayerAttack,
     EnemyAttack
 }
