d1f7ee66341fa8b7b37cb96f6a62b060d37fab5d
mikenchi <mikenti337564@gmail.com>
2024-08-18 13:46:38 +0900

    カードベースの設計を分けることにしました

--- Assets/tsutsumi/Script/CardBase.cs
+++ Assets/tsutsumi/Script/CardBase.cs
@@ -1,128 +1,54 @@
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CardBase : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler
+public class CardBase : MonoBehaviour
 {
-    public static bool _isPlayer = true;
-    GameObject _desk;
-    GameObject _nulldesk;
-    GameObject _selectdesk;
-    CharacterBase _player;
     [SerializeField] BuffDebuff _isBuff;
     [SerializeField] Buff _buff;
     [SerializeField] int _attackCount = 1;
-    [SerializeField] bool _test = true;
     [SerializeField] string _dictionary = "説明なし";
     [SerializeField] int _price = 1;
-    void Start()
+    public BuffDebuff CardUse(CharacterBase attack , CharacterBase beattacked)
     {
-        _desk = GameObject.FindWithTag("Desk");
-        _nulldesk = GameObject.FindWithTag("DeskOut");
-        _selectdesk = GameObject.FindWithTag("SelectDesk");
-        if (!_test)
-        {
-            _player = GameObject.FindWithTag("Player").GetComponent<CharacterBase>();
-        }
-        transform.SetParent(_desk.transform);
-    }
-    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
-    {
-        if (_isPlayer)
-        {
-            transform.SetParent(_nulldesk.transform);
-        }
-    }
-    void IDragHandler.OnDrag(PointerEventData eventData)
-    {
-        if (_isPlayer)
-        {
-            transform.position = eventData.position;
-        }
-        else if(transform.parent.tag =="DeskOut")
-        {
-            transform.SetParent(_desk.transform);
-        }
-    }
-    void IEndDragHandler.OnEndDrag(PointerEventData eventData)
-    {
-        if (_isPlayer)
-        {
-            GameObject desk = GetCurrentDeck(eventData);
-            if (desk)
-            {
-                transform.SetParent(desk.transform);
-            }
-            else
-            {
-                transform.SetParent(_selectdesk.transform);
-            }
-        }
-    }
-
-    GameObject GetCurrentDeck(PointerEventData eventData)
-    {
-        var results = new List<RaycastResult>();
-        EventSystem.current.RaycastAll(eventData, results);
-        RaycastResult result = default;
-        foreach (var item in results)
-        {
-            if (item.gameObject.CompareTag("Desk"))
-            {
-                result = item;
-                break;
-            }
-        }
-        return result.gameObject;
-    }
-    public BuffDebuff CardUse(CharacterBase enemy)
-    {
-        Debug.Log($"カードを{enemy}へ使用");
-        CardUsing(enemy);
+        Debug.Log($"カードを{attack}へ使用");
+        CardUsing(attack,beattacked);
         CardUseEvent();
         return _isBuff;
     }
-    public void CardUsing(CharacterBase enemy)
+    public void CardUsing(CharacterBase attack,CharacterBase beattacked)
     {
-        if (_isBuff == BuffDebuff.OverRide)
+        switch (_isBuff)
         {
+            case BuffDebuff.OverRide:
             CardOverRide();
+            break;
+            case BuffDebuff.Attack:
+            attack.Attack(beattacked);
+            break;
+            case BuffDebuff.Buff:
+            attack._buff.Add(_buff);
+            break;
+            case BuffDebuff.OneTimeBuff:
+            attack._oneTimeBuff.Add(_buff);
+            break;
+            case BuffDebuff.AllAttack:
+            attack._attackPattern = AttackPattern.All;
+            break;
+            case BuffDebuff.AttackCount:
+            attack._attackCount = _attackCount;
+            break;
         }
-        else if (_isBuff == BuffDebuff.Attack)
-        {
-            enemy.Attack(_player);
-        }
-        else if (_isBuff == BuffDebuff.Buff)
-        {
-            _player._buff.Add(_buff);
-        }
-        else if (_isBuff == BuffDebuff.OneTimeBuff)
-        {
-            _player._oneTimeBuff.Add(_buff);
-        }
-        else if (_isBuff == BuffDebuff.AllAttack)
-        {
-            _player._attackPattern = AttackPattern.All;
-        }
-        else if (_isBuff == BuffDebuff.AttackCount)
-        {
-            _player._attackCount = _attackCount;
-        }
-        else if (_isBuff == BuffDebuff.Debuff)
-        {
-        }
-
     }
     public virtual void CardOverRide()
     {
-
+        Debug.Log("オーバーライドされていません");
     }
     public virtual void CardUseEvent()
     {
         Destroy(gameObject);
     }
-
-
 }
 public enum BuffDebuff
 {
