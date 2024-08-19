68ea64633e9906ad0e478164f865af881c8ff544
kobakoGlhf <147145211+kobakoGlhf@users.noreply.github.com>
2024-08-19 21:30:27 +0900

    [add]敵が同士討ちするデバフの実行部分の作成(デバフのenum待ち)、カードのポップアウトの挙動修正

--- Assets/Kobayashi/Script/StoreCard.cs
+++ Assets/Kobayashi/Script/StoreCard.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoreCard : MonoBehaviour
 {
+    [SerializeField]Text _priceText;
+    [SerializeField]Text _cardInstructionText;
     [HideInInspector]public CardBase CardData;
-    public void OnClickCallStoreMethod()
+    Transform _canvas;
+    private void Start()
+    {
+        _canvas = transform.root;
+        var cardBase = GetComponent<CardBase>();
+        _priceText.text = cardBase._price.ToString();
+        _cardInstructionText.text = cardBase._dictionary;
+    }
+    public void CallStoreMethodOnClick()
     {
         Store.Instance.CardOnClick(CardData);
+        Destroy(_cardInstructionText.gameObject);
         Destroy(gameObject);
+    }
+    public void PopOutText(bool popOut)
+    {
+        if(popOut)_cardInstructionText.transform.SetParent(transform);
+        if (!popOut) _cardInstructionText.transform.SetParent(_canvas);
     }
 }
